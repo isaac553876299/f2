ac3ed7870111c1735626426e8a50c19ba440b761
isaacdd <isaacdd.bcn.2001@gmail.com>
2021-01-14 16:31:29 +0100

    uff

---
+++ Code/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../SDL2-2.0.14/include/SDL.h"
+
+int main(int argc, char** argv)
+{
+
+	return 0;
+}
--- f2/Code/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "../SDL2-2.0.14/include/SDL.h"
-
-int main(int argc, char** argv)
-{
-
-	return 0;
-}
