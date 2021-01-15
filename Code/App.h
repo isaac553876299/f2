de8c1ad4a87fd8f48c47a312c7dfbe5ce8c385ce
isaacdd <isaacdd.bcn.2001@gmail.com>
2021-01-15 01:38:38 +0100

    gotta fix dt

--- Code/App.h
+++ Code/App.h
@@ -11,22 +11,33 @@ public:
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
 
-	float dt = SDL_GetTicks();
+	float dt;
+	Uint32 seconds;
+	int fpsCount;
+	int fps;
 
 	//KeyStates::IDLE_0,DOWN_1,REPEAT_2,UP_3
-	mouse _mouse{ 0,0,0,0,0,0 };
+	mouse _mouse;
 	int keyboard[200];
+
 	//=========================================================================================
 	Physics physicsBox;
 	//=========================================================================================
+
 	App()
 	{
 		SDL_Init(SDL_INIT_VIDEO);
 		SDL_CreateWindowAndRenderer(WW, WH, 0, &window, &renderer);
 
+		_mouse = { 0,0,0,0,0,0 };
+
 		for (auto i : keyboard) i = 0;
 		for (int i = 0; i < 200; i++) keyboard[i] = 0;
 
+		dt = SDL_GetTicks() / 1000;
+		seconds = SDL_GetTicks();
+		fpsCount = 0;
+		fps = 0;
 	}
 
 	~App()
@@ -61,7 +72,6 @@ public:
 		const Uint8* keyboardState = SDL_GetKeyboardState(0);
 		if (keyboardState[SDL_SCANCODE_ESCAPE]) quit = true;
 
-		//IDLE_0,DOWN_1,REPEAT_2,UP_3
 		for (int i = 0; i < 200; i++)
 		{
 			if (keyboard[i] == 3) keyboard[i] = 0;
@@ -77,18 +87,29 @@ public:
 	void Update()
 	{
 		dt = (SDL_GetTicks() / 1000) - dt;
+
+		fpsCount++;
+		if (SDL_GetTicks() - seconds > 1000)
+		{
+			seconds = SDL_GetTicks();
+			fps = fpsCount;
+			fpsCount = 0;
+		}
+
 		//=========================================================================================
 		physicsBox.Input(_mouse, keyboard);
 		physicsBox.Update(dt);
 		//=========================================================================================
+
 		static char title[256];
-		sprintf_s(title, 256, "mouse{%d,%d,%d,%d} // dt(%.4f) fps(%.4f)",
-			_mouse.x, _mouse.y, _mouse.stateL, _mouse.stateR, dt, (1000 / (dt+1)));
+		sprintf_s(title, 256, "fps(%d) | mouse{%d,%d,%d,%d}",
+			fps, _mouse.x, _mouse.y, _mouse.stateL, _mouse.stateR);
 		SDL_SetWindowTitle(window, title);
 
-		Uint32 diff = SDL_GetTicks() - dt;
-		Uint32 delay = (diff < (1000 / FPS)) ? (1000 / FPS) - diff : 0;
-		SDL_Delay(delay);
+		//Uint32 _dt = SDL_GetTicks() - 1000 * dt;
+		//Uint32 delay = ((1000 / FPS) - _dt > 0) ? ((1000 / FPS) - _dt) : 0;
+		//SDL_Delay(delay);
+		// (edt - dt > 0)(dt < edt)
 	}
 
 	void Draw()
@@ -96,9 +117,11 @@ public:
 		SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
+
 		//=========================================================================================
 		physicsBox.Draw(renderer);
 		//=========================================================================================
+
 		SDL_RenderPresent(renderer);
 	}
 };
--- Code/defs.h
+++ Code/defs.h
@@ -3,17 +3,20 @@
 #include <iostream>
 #include "../SDL2-2.0.14/include/SDL.h"
 
+#define FPS 60
 #define WW 1280
 #define WH 720
-#define FPS 1
+#define RAD(a) (a*3.14/180)
+
+#define RELEASE(x) { delete x; x = nullptr; }
 
 struct mouse { int x, y, offsx, offsy, stateL, stateR; };
 
-#define RAD(a) (a*3.14/180)
-
 struct point { int a, b; };
+struct fpoint { float a, b; };
 struct rect { int a, b, c, d; };
 
 inline bool PointInRect(point p, rect r) { return (p.a > r.a && p.a < r.a + r.c && p.b > r.b && p.b < r.b + r.d); }
+inline bool fPointInRect(fpoint p, rect r) { return (p.a > r.a && p.a < r.a + r.c && p.b > r.b && p.b < r.b + r.d); }
 
 #endif
